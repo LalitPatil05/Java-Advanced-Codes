37cb66273b44143210f0dd6a7c25b5e296ae3ae0
LalitPatil05 <lalitpatil4636@gmail.com>
2026-02-24 01:06:16 +0530

    [ADDED] : Lalit -Problem Solving for Day 23

---
+++ SolutionDay23.java
@@ -0,0 +1,38 @@
+public class SolutionDay23 {
+
+    public static String addBinary(String a, String b) {
+
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 2); // current bit
+            carry = sum / 2;        // next carry
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String args[]) {
+        String str1 = "11";
+        String str2 = "1";
+
+        System.out.println("Output is : "+addBinary(str1, str2));
+    }
+}
